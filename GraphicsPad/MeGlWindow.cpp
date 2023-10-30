192873ba55d720f46cb8d892b446592426723fde
Tianji <111904203+FelixFelicis24@users.noreply.github.com>
2023-10-30 15:00:35 -0600

    Camera

--- GraphicsPad/MeGlWindow.cpp
+++ GraphicsPad/MeGlWindow.cpp
@@ -115,33 +115,33 @@ void MeGlWindow::paintGL()
 
 void MeGlWindow::mouseMoveEvent(QMouseEvent* e)
 {
-	//camera.mouseUpdate(glm::vec2(e->x(), e->y()));
+	camera.mouseUpdate(glm::vec2(e->x(), e->y()));
 	repaint();
 }
 
 void MeGlWindow::keyPressEvent(QKeyEvent* e)
 {
-	//switch (e->key())
-	//{
-	//case Qt::Key::Key_W:
-	//	camera.moveForward();
-	//	break;
-	//case Qt::Key::Key_S:
-	//	camera.moveBackward();
-	//	break;
-	//case Qt::Key::Key_A:
-	//	camera.strafeLeft();
-	//	break;
-	//case Qt::Key::Key_D:
-	//	camera.strafeRight();
-	//	break;
-	//case Qt::Key::Key_R:
-	//	camera.moveUp();
-	//	break;
-	//case Qt::Key::Key_F:
-	//	camera.moveDown();
-	//	break;
-	//}
+	switch (e->key())
+	{
+	case Qt::Key::Key_W:
+		camera.moveForward();
+		break;
+	case Qt::Key::Key_S:
+		camera.moveBackward();
+		break;
+	case Qt::Key::Key_A:
+		camera.strafeLeft();
+		break;
+	case Qt::Key::Key_D:
+		camera.strafeRight();
+		break;
+	case Qt::Key::Key_R:
+		camera.moveUp();
+		break;
+	case Qt::Key::Key_F:
+		camera.moveDown();
+		break;
+	}
 	repaint();
 }
 
